4c2e5fc3ea81e7a2f09035a87e716caf56ca8a42
John DoeSam <sam@sparttech.co.uk>
2025-01-17 21:58:07 +0000

    Fix base path and redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ACLIndustries/', // Ensure this matches your repository name
+  base: '/', // Ensure this matches your repository name
   plugins: [react()],
 });
